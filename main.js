398c489523d7e7779cd7391c7340f04804a0e48c
Antonnn666 <antonv1997@gmail.com>
2023-10-29 12:54:05 +0200

    Add function and user input to play round of rock paper scissors

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const options = ["rock", "paper", "scissors"];
 
-function getComputerChoise() {
+function getComputerChoice() {
     return options.at(Math.random() * 3);
 }
 
-console.log(getComputerChoise());
+function playRound (playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
+        return `It's a tie! You both selected ${playerSelection}`;
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else return `You lose :(! ${computerSelection} beats ${playerSelection}`;
+}
+
+const playerSelection = prompt("Select rock, paper or scissors:").toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(playRound(playerSelection, computerSelection));
